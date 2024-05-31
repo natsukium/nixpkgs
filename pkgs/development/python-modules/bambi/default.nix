5c32ac853df158c63367de65b6d577ca8e83df3f
natsukium <tomoya.otabi@gmail.com>
2024-05-31 22:30:17 +0900

    python312Packages.bambi: disable tests with pytest-sandbox

--- pkgs/development/python-modules/bambi/default.nix
+++ pkgs/development/python-modules/bambi/default.nix
@@ -11,6 +11,7 @@
   pymc,
   blackjax,
   numpyro,
+  pytest-sandbox,
   pytestCheckHook,
 }:
 
@@ -45,38 +46,13 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     blackjax
     numpyro
+    pytest-sandbox
     pytestCheckHook
   ];
 
   disabledTests = [
-    # Tests require network access
-    "test_alias_equal_to_name"
-    "test_average_by"
-    "test_ax"
-    "test_basic"
-    "test_censored_response"
-    "test_custom_prior"
-    "test_data_is_copied"
-    "test_distributional_model"
-    "test_elasticity"
-    "test_extra_namespace"
-    "test_fig_kwargs"
-    "test_gamma_with_splines"
-    "test_group_effects"
-    "test_hdi_prob"
-    "test_legend"
-    "test_non_distributional_model"
-    "test_normal_with_splines"
-    "test_predict_offset"
+    # TypeError: Only tensors with the same number of dimensions can be joined.
     "test_predict_new_groups"
-    "test_predict_new_groups_fail"
-    "test_set_alias_warnings"
-    "test_subplot_kwargs"
-    "test_transforms"
-    "test_use_hdi"
-    "test_with_groups"
-    "test_with_group_and_panel"
-    "test_with_user_values"
   ];
 
   pythonImportsCheck = [ "bambi" ];
