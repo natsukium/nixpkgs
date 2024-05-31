616333bccb1a3f4aad6ed545889a0ee77b364cf9
natsukium <tomoya.otabi@gmail.com>
2024-05-31 21:14:22 +0900

    python312Packages.shap: disable tests with pytest-sandbox

--- pkgs/development/python-modules/shap/default.nix
+++ pkgs/development/python-modules/shap/default.nix
@@ -19,6 +19,7 @@
   pandas,
   pyspark,
   pytest-mpl,
+  pytest-sandbox,
   scikit-learn,
   scipy,
   sentencepiece,
@@ -69,47 +70,17 @@ buildPythonPackage rec {
     others = [ lime ];
   };
 
-  preCheck =
-    let
-      # This pytest hook mocks and catches attempts at accessing the network
-      # tests that try to access the network will raise, get caught, be marked as skipped and tagged as xfailed.
-      conftestSkipNetworkErrors = writeText "conftest.py" ''
-        from _pytest.runner import pytest_runtest_makereport as orig_pytest_runtest_makereport
-        import urllib, requests, transformers
-
-        class NetworkAccessDeniedError(RuntimeError): pass
-        def deny_network_access(*a, **kw):
-          raise NetworkAccessDeniedError
-
-        requests.head = deny_network_access
-        requests.get  = deny_network_access
-        urllib.request.urlopen = deny_network_access
-        urllib.request.Request = deny_network_access
-        transformers.AutoTokenizer.from_pretrained = deny_network_access
-
-        def pytest_runtest_makereport(item, call):
-          tr = orig_pytest_runtest_makereport(item, call)
-          if call.excinfo is not None and call.excinfo.type is NetworkAccessDeniedError:
-              tr.outcome = 'skipped'
-              tr.wasxfail = "reason: Requires network access."
-          return tr
-      '';
-    in
-    ''
-      export HOME=$TMPDIR
-      # when importing the local copy the extension is not found
-      rm -r shap
-
-      # Add pytest hook skipping tests that access network.
-      # These tests are marked as "Expected fail" (xfail)
-      cat ${conftestSkipNetworkErrors} >> tests/conftest.py
-    '';
+  preCheck = ''
+    export HOME=$TMPDIR
+    rm -r shap
+  '';
 
   nativeCheckInputs = [
     ipython
     matplotlib
     nose
     pytest-mpl
+    pytest-sandbox
     pytestCheckHook
     # optional dependencies, which only serve to enable more tests:
     catboost
