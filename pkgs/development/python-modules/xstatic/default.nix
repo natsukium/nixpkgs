22583126d0dd6590db4ffade3c1188081d7f666b
natsukium <tomoya.otabi@gmail.com>
2024-07-14 16:48:46 +0900

    python312Packages.xstatic: adopt pypa builder

--- pkgs/development/python-modules/xstatic/default.nix
+++ pkgs/development/python-modules/xstatic/default.nix
@@ -1,5 +1,6 @@
 {
   buildPythonPackage,
+  setuptools,
   lib,
   fetchPypi,
 }:
@@ -7,6 +8,10 @@
 buildPythonPackage rec {
   pname = "xstatic";
   version = "1.0.3";
+
+  pyproject = true;
+
+  build-system = [ setuptools ];
 
   src = fetchPypi {
     pname = "XStatic";
