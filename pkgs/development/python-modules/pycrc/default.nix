8b11d4477214f5326d8e71529fdbf1f384bcdb6e
natsukium <tomoya.otabi@gmail.com>
2024-07-14 16:48:34 +0900

    python312Packages.pycrc: adopt pypa builder

--- pkgs/development/python-modules/pycrc/default.nix
+++ pkgs/development/python-modules/pycrc/default.nix
@@ -1,12 +1,17 @@
 {
   lib,
   buildPythonPackage,
+  setuptools,
   fetchPypi,
 }:
 
 buildPythonPackage rec {
   pname = "pycrc";
   version = "1.21";
+
+  pyproject = true;
+
+  build-system = [ setuptools ];
 
   src = fetchPypi {
     pname = "PyCRC";
