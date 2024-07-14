efc173a2c4a28216335dd7d6537d3532b62ddac0
natsukium <tomoya.otabi@gmail.com>
2024-07-14 16:48:25 +0900

    python312Packages.foxdot: adopt pypa builder

--- pkgs/development/python-modules/foxdot/default.nix
+++ pkgs/development/python-modules/foxdot/default.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   buildPythonPackage,
+  setuptools,
   fetchPypi,
   tkinter,
   supercollider,
@@ -11,13 +12,17 @@ buildPythonPackage rec {
   pname = "foxdot";
   version = "0.8.12";
 
+  pyproject = true;
+
+  build-system = [ setuptools ];
+
   src = fetchPypi {
     pname = "FoxDot";
     inherit version;
     sha256 = "528999da55ad630e540a39c0eaeacd19c58c36f49d65d24ea9704d0781e18c90";
   };
 
-  propagatedBuildInputs =
+  dependencies =
     [ tkinter ]
     # we currently build SuperCollider only on Linux
     # but FoxDot is totally usable on macOS with the official SuperCollider binary
