dc678000dfdb917ccd30423ac4210db624f17f9f
natsukium <tomoya.otabi@gmail.com>
2024-07-14 16:48:19 +0900

    python312Packages.cjkwrap: adopt pypa builder

--- pkgs/development/python-modules/cjkwrap/default.nix
+++ pkgs/development/python-modules/cjkwrap/default.nix
@@ -1,12 +1,17 @@
 {
   lib,
   buildPythonPackage,
+  setuptools,
   fetchPypi,
 }:
 
 buildPythonPackage rec {
   pname = "cjkwrap";
   version = "2.2";
+  pyproject = true;
+
+  build-system = [ setuptools ];
+
   src = fetchPypi {
     pname = "CJKwrap";
     inherit version;
