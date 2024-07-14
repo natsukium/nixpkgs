bad5b86187ad3fc6d5442f053fab516e2d223e67
natsukium <tomoya.otabi@gmail.com>
2024-07-14 16:48:56 +0900

    python312Packages.zope-copy: adopt pypa builder

--- pkgs/development/python-modules/zope-copy/default.nix
+++ pkgs/development/python-modules/zope-copy/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   buildPythonPackage,
+  setuptools,
   fetchPypi,
   isPy27,
   zope-interface,
@@ -13,12 +14,16 @@ buildPythonPackage rec {
   pname = "zope.copy";
   version = "4.3";
 
+  pyproject = true;
+
+  build-system = [ setuptools ];
+
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-epg2yjqX9m1WGzYPeGUBKGif4JNAddzg75ECe9xPOlc=";
   };
 
-  propagatedBuildInputs = [ zope-interface ];
+  dependencies = [ zope-interface ];
 
   doCheck = !isPy27; # namespace conflicts
   nativeCheckInputs = [
