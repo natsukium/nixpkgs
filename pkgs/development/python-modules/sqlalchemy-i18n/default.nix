b9cc859eb53d41e94fbef09fe357e07bb28d7037
natsukium <tomoya.otabi@gmail.com>
2024-07-14 16:48:42 +0900

    python312Packages.sqlalchemy-i18n: adopt pypa builder

--- pkgs/development/python-modules/sqlalchemy-i18n/default.nix
+++ pkgs/development/python-modules/sqlalchemy-i18n/default.nix
@@ -2,6 +2,7 @@
   lib,
   fetchPypi,
   buildPythonPackage,
+  setuptools,
   sqlalchemy,
   sqlalchemy-utils,
   six,
@@ -11,13 +12,17 @@ buildPythonPackage rec {
   pname = "sqlalchemy-i18n";
   version = "1.1.0";
 
+  pyproject = true;
+
+  build-system = [ setuptools ];
+
   src = fetchPypi {
     pname = "SQLAlchemy-i18n";
     inherit version;
     sha256 = "de33376483a581ca14218d8f57a114466c5f72b674a95839b6c4564a6e67796f";
   };
 
-  propagatedBuildInputs = [
+  dependencies = [
     sqlalchemy
     sqlalchemy-utils
     six
