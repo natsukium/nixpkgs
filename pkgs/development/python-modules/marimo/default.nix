82e9cbaf92a46a2b55d23a118cc8660a2db628c3
natsukium <tomoya.otabi@gmail.com>
2024-11-01 02:33:08 +0900

    python312Packages.marimo: 0.9.1 -> 0.9.14
    Changelog: https://github.com/marimo-team/marimo/releases/tag/0.9.14

--- pkgs/development/python-modules/marimo/default.nix
+++ pkgs/development/python-modules/marimo/default.nix
@@ -24,14 +24,14 @@
 
 buildPythonPackage rec {
   pname = "marimo";
-  version = "0.9.1";
+  version = "0.9.14";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-7sG6i1WusNuCpuaojFXhSfPWCA2/nzHXvMF29ApGTDg=";
+    hash = "sha256-Q3dnRuAS8B4cWvF04GGg5OOZtmAJPKa2fHwnoO2DXDs=";
   };
 
   build-system = [ setuptools ];
