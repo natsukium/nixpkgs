c8183e2087b232045248ee0fddbd0d09bbb9884d
natsukium <tomoya.otabi@gmail.com>
2024-11-01 02:30:16 +0900

    python312Packages.jupysql: 0.10.13 -> 0.10.14
    Diff: https://github.com/ploomber/jupysql/compare/refs/tags/0.10.13...0.10.14
    
    Changelog: https://github.com/ploomber/jupysql/blob/0.10.14/CHANGELOG.md

--- pkgs/development/python-modules/jupysql/default.nix
+++ pkgs/development/python-modules/jupysql/default.nix
@@ -36,7 +36,7 @@
 
 buildPythonPackage rec {
   pname = "jupysql";
-  version = "0.10.13";
+  version = "0.10.14";
 
   pyproject = true;
 
@@ -44,7 +44,7 @@ buildPythonPackage rec {
     owner = "ploomber";
     repo = "jupysql";
     rev = "refs/tags/${version}";
-    hash = "sha256-vNuMGHFkatJS5KjxaOBwZ7JolIDAdYqGq3JNKSV2fKE=";
+    hash = "sha256-UqbAFrpFHYaEvPRoFfxZwWucgUhls8EB0o3vDjnGG7s=";
   };
 
   pythonRelaxDeps = [ "sqlalchemy" ];
